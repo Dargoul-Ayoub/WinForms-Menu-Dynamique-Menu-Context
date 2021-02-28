4f5e2e366a3f348c4d9f6bb04320cdcdcf89e912
infoayoub <infoayoub@github.com>
2021-02-28 12:14:18 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace _6._3.Menus_Dynamiques
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.prenomsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.policeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem,
+            this.prenomsToolStripMenuItem,
+            this.policeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(428, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fichierToolStripMenuItem
+            // 
+            this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitterToolStripMenuItem});
+            this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fichierToolStripMenuItem.Text = "File";
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitterToolStripMenuItem.Text = "Close";
+            this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
+            // 
+            // prenomsToolStripMenuItem
+            // 
+            this.prenomsToolStripMenuItem.Name = "prenomsToolStripMenuItem";
+            this.prenomsToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.prenomsToolStripMenuItem.Text = "Names";
+            // 
+            // policeToolStripMenuItem
+            // 
+            this.policeToolStripMenuItem.Name = "policeToolStripMenuItem";
+            this.policeToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.policeToolStripMenuItem.Text = "Fonts";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 211);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(428, 38);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = global::_6._3.Menus_Dynamiques.Properties.Resources.engineer;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(48, 35);
+            this.toolStripButton1.Text = "Names";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = global::_6._3.Menus_Dynamiques.Properties.Resources.signs;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(40, 35);
+            this.toolStripButton2.Text = "Fonts";
+            this.toolStripButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = global::_6._3.Menus_Dynamiques.Properties.Resources.signs__1_;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(40, 35);
+            this.toolStripButton3.Text = "Close";
+            this.toolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(428, 187);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 249);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem prenomsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem policeToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6._3.Menus_Dynamiques
+{
+
+    public partial class Form1 : Form
+    {
+        List<String> ListPrenoms = new List<String>{"C#", "Java", "Python", "WinForms", "WPF", "Algo"};
+        ToolStripMenuItem PolicemenuItem;  //6.4. Menus Contextuels
+        ToolStripMenuItem ColorMenuItem;  //6.4. Menus Contextuels
+        ContextMenuStrip menuStrip;  //6.4. Menus Contextuels
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (toolStripButton1.Enabled)
+            {
+                prenomsToolStripMenuItem.DropDownItems.Clear();
+                prenomsToolStripMenuItem.DropDownItems.Add("C#").Click += new EventHandler(this.AyoubToolStripMenuItemView_Click); // i made a handler for the toolstripmenuitems i have added 
+                prenomsToolStripMenuItem.DropDownItems.Add("Java").Click += new EventHandler(this.AyoubToolStripMenuItemView_Click);
+                prenomsToolStripMenuItem.DropDownItems.Add("Python").Click += new EventHandler(this.AyoubToolStripMenuItemView_Click);
+                prenomsToolStripMenuItem.DropDownItems.Add("Winforms").Click += new EventHandler(this.AyoubToolStripMenuItemView_Click);
+                prenomsToolStripMenuItem.DropDownItems.Add("WPF").Click += new EventHandler(this.AyoubToolStripMenuItemView_Click);
+                toolStripButton1.Enabled = false;
+            }
+            else
+            {
+                prenomsToolStripMenuItem.DropDownItems.Clear();
+                prenomsToolStripMenuItem.DropDownItems.Add(" ");
+                toolStripButton1.Enabled = true;
+            }
+
+        }
+
+        private void AyoubToolStripMenuItemView_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;// sender it provides a reference  to the object that raised this event ,
+            // so i have 5 reference so eache one is  bound at its reference
+            // because sender it's an object nullabl so i definded him like a ToolStripMenuItem using as 
+            label1.Text = item.Text;
+            foreach(ToolStripMenuItem tool in prenomsToolStripMenuItem.DropDownItems)
+            {
+                tool.Checked = false;
+            }
+            // first i made all checked items false , so when i click an item it will that only  wich is checked 
+            item.Checked = true;
+        }
+
+        
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (toolStripButton2.Enabled == true)
+            {
+                policeToolStripMenuItem.DropDownItems.Clear();
+                foreach (var font in FontFamily.Families)
+                {
+                    ToolStripMenuItem subItem = new ToolStripMenuItem(font.Name);
+                    subItem.Click += new EventHandler(this.policeViex_Click);
+                    policeToolStripMenuItem.DropDownItems.Add(subItem);
+                }
+
+                toolStripButton2.Enabled = false;
+            }
+            else
+            {
+                policeToolStripMenuItem.DropDownItems.Clear();
+                policeToolStripMenuItem.DropDownItems.Add(" ");
+                toolStripButton2.Enabled = true;
+            }
+
+        }
+
+        private void policeViex_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+            FontFamily font = new FontFamily(item.Text);
+            label1.Font =new Font(font,label1.Font.Size);
+            foreach (ToolStripMenuItem tool in policeToolStripMenuItem.DropDownItems)
+            {
+                tool.Checked = false;
+            }
+            item.Checked = true;
+            PolicemenuItem.DropDownItems.Add(item.Text).Click += new EventHandler(PolicepopupMenu_Click);  //6.4. Menus Contextuels
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            prenomsToolStripMenuItem.DropDownItems.Add(" ");
+            policeToolStripMenuItem.DropDownItems.Add(" ");
+            CreateContextMenu();
+        }
+
+        //6.4. Menus Contextuels
+        private void CreateContextMenu()
+
+        {
+            menuStrip = new ContextMenuStrip();
+            PolicemenuItem = new ToolStripMenuItem("Fonts");
+            ColorMenuItem = new ToolStripMenuItem("Colors");
+             ColorMenuItem.DropDownItems.Add("Red").Click+=new EventHandler(ColorspopupMenu_Click);
+             ColorMenuItem.DropDownItems.Add("Green").Click += new EventHandler(ColorspopupMenu_Click);
+             ColorMenuItem.DropDownItems.Add("Bleu").Click += new EventHandler(ColorspopupMenu_Click);
+             ColorMenuItem.DropDownItems.Add("White").Click += new EventHandler(ColorspopupMenu_Click);
+             ColorMenuItem.DropDownItems.Add("Yellow").Click += new EventHandler(ColorspopupMenu_Click);
+
+          /*  foreach (KnownColor font in Enum.GetValues(typeof(KnownColor)))
+            {
+                Color color = Color.FromKnownColor(font);
+                ToolStripMenuItem subItem = new ToolStripMenuItem(color.Name);
+                subItem.Click += new EventHandler(ColorspopupMenu_Click);
+                ColorMenuItem.DropDownItems.Add(subItem);
+            }*/
+
+
+            PolicemenuItem.DropDownItems.Add(" ");
+            menuStrip.Items.Add(PolicemenuItem);
+            menuStrip.Items.Add(ColorMenuItem);
+            ContextMenuStrip = menuStrip;
+            
+        }
+
+        //6.4. Menus Contextuels
+        private void ColorspopupMenu_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+            switch (item.Text)
+            {
+                case "Red":
+                    label1.ForeColor = Color.Red;
+                    break;
+                case "Green":
+                    label1.ForeColor = Color.Green;
+                    break;
+                case "Bleu":
+                    label1.ForeColor = Color.Blue;
+                    break;
+                case "White":
+                    label1.ForeColor = Color.White;
+                    break;
+                case "Yellow":
+                    label1.ForeColor = Color.Yellow;
+                    break;
+            }
+            foreach (ToolStripMenuItem tool in ColorMenuItem.DropDownItems)
+            {
+                tool.Checked = false;
+            }
+            item.Checked = true;
+        }
+
+        //6.4. Menus Contextuels
+        private void PolicepopupMenu_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = sender as ToolStripMenuItem;
+            FontFamily font = new FontFamily(item.Text);
+            label1.Font = new Font(font, label1.Font.Size);
+            foreach (ToolStripMenuItem tool in PolicemenuItem.DropDownItems)
+            {
+                tool.Checked = false;
+            }
+            item.Checked = true;
+        }
+    }
+
+
+}
+
